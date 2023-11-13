5767098065f1016bad2d7d5fbdf0272e8348fdbe
Hasim Mehmood <hmehmood5365@hwdsb.on.ca>
2023-11-13 18:58:42 -0500

    Animations for button click, all buttons Not very modular, later I'll make it use the pygame rect values Harcoded the coords for passing thorugh to play_anim

--- main.py
+++ main.py
@@ -16,20 +16,26 @@ hit_button= pygame.image.load("Assets\Buttons\Button\Hit_Button.png")
 stand_button= pygame.image.load("Assets\Buttons\Button\Stand_Button.png")
 new_game_button= pygame.image.load("Assets\Buttons\Button/NewGame_Button.png")
 back_of_card= pygame.image.load("Assets\Cards\Back_of_Card.png")
-
+animations = ["New Game", "Hit", "Stand"]
 # Define the ranks and suits for the deck of cards
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
-# Load the "New Game" gif using Pillow
-new_game_animation = Image.open("Assets\Buttons\Click Animation/New Game.gif")
-new_game_animation_frames = [frame.copy().convert('RGB') for frame in ImageSequence.Iterator(new_game_animation)]
+# Create a dictionary to store animation frames
+animation_frames = {}
+
+# Load animations using Pillow and store frames in the dictionary
+for animation_name in animations:
+    animation_path = f"Assets/Buttons/Click Animation/{animation_name}.gif"
+    animation = Image.open(animation_path)
+    frames = [frame.copy().convert('RGB') for frame in ImageSequence.Iterator(animation)]
+    animation_frames[animation_name] = frames
+    animation_width, animation_height = animation_frames[animation_name][0].size
 
 # Get the size of the first frame to determine the dimensions
-new_game_animation_width, new_game_animation_height = new_game_animation_frames[0].size
 
-def play_anim(anim_name, xcoord, ycoord ):
-    for frame in anim_name:
+def play_anim(anim_name, xcoord, ycoord):
+    for frame in animation_frames[anim_name]:
         pygame_frame = pygame.image.fromstring(frame.tobytes(), frame.size, 'RGB')
         screen.blit(pygame_frame, (xcoord, ycoord))
         pygame.display.flip()
@@ -116,11 +122,13 @@ while running:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if hit_button_rect.collidepoint(event.pos):
                     # The "Hit" button was clicked, so draw a card and add it to the player's hand.
+                    play_anim("Hit", 20, 500)
                     if not deck:
                         reset_deck()  # If the deck is empty, reshuffle the cards.
                     player_hand.append(deck.pop())  # Draw a card from the deck.
                 elif stand_button_rect.collidepoint(event.pos):
                     # The "Stand" button was clicked, so the player's turn is over. Or the value is over 21.
+                    play_anim("Stand", 140, 500)
                     dealer_draw()
                     game_over = True  # The game is now over.
 
@@ -128,7 +136,7 @@ while running:
             if new_game_button_rect.collidepoint(event.pos):
                 # The "New Game" button was clicked, so reset the game by reshuffling the deck
                 # and dealing new hands for both the player and the dealer.
-                play_anim(new_game_animation_frames, 260,500)
+                play_anim("New Game", 260,500)
                 pygame.display.flip()
                 pygame.time.delay(50)  # Display the animation for 1000 milliseconds (adjust as needed)
                 reset_deck()  # If the deck is empty, reshuffle the cards.
